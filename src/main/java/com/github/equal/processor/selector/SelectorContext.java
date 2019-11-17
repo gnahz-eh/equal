62466326fa6cfebd63c47f0f79f616f2ee097766
gnahZ-eH <hershxy@sjtu.edu.cn>
2019-11-17 15:50:21 +0800

    Modify: Move file-methods into FileUtils.

--- src/main/java/com/github/equal/processor/selector/SelectorContext.java
+++ src/main/java/com/github/equal/processor/selector/SelectorContext.java
@@ -27,10 +27,7 @@ package com.github.equal.processor.selector;
 import com.github.equal.enums.FileType;
 import com.github.equal.utils.ExceptionUtils;
 import com.github.equal.exception.EqualException;
-import com.github.equal.utils.StringUtils;
-import org.apache.poi.poifs.filesystem.FileMagic;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import com.github.equal.utils.FileUtils;
 
 import java.io.*;
 import java.util.stream.Stream;
@@ -40,11 +37,11 @@ public class SelectorContext {
     private static FileSelector fileSelector;
 
     public static <T> Stream<T> selectFromFile(Selector selector) throws EqualException {
-        FileType fileType = getFileTypeBySourceFile(selector.getSourceFile());
+        FileType fileType = FileUtils.getFileTypeBySourceFile(selector.getSourceFile());
         switch (fileType) {
             case XLSX:
             case XLS:
-                fileSelector = new XLSSelector(getWorkbook(selector.getSourceFile()));
+                fileSelector = new XLSSelector(FileUtils.getWorkbook(selector.getSourceFile()));
                 break;
             case CSV:
                 try {
@@ -62,15 +59,15 @@ public class SelectorContext {
     public static <T> Stream<T> selectFromStream(Selector selector) throws EqualException {
         byte[] bytes = null;
         try {
-            bytes = toByteArray(selector.getInputStream());
+            bytes = FileUtils.toByteArray(selector.getInputStream());
         } catch (IOException e) {
             throw new EqualException(e);
         }
-        FileType fileType = getFileTypeByInputStream(selector.getInputStream());
+        FileType fileType = FileUtils.getFileTypeByInputStream(selector.getInputStream());
         switch (fileType) {
             case XLSX:
             case XLS:
-                fileSelector = new XLSSelector(getWorkbook(new ByteArrayInputStream(bytes)));
+                fileSelector = new XLSSelector(FileUtils.getWorkbook(new ByteArrayInputStream(bytes)));
                 break;
             case CSV:
                 fileSelector = new CSVSelector(new ByteArrayInputStream(bytes));
@@ -79,82 +76,5 @@ public class SelectorContext {
                 throw new EqualException(ExceptionUtils.UNSUPPORTED_FILE_TYPE);
         }
         return fileSelector.selectFromFile(selector);
-    }
-
-    public static FileType getFileTypeBySourceFile(File sourceFile) {
-        if (sourceFile == null || !sourceFile.exists()) {
-            return FileType.NONE;
-        }
-        String fileNameExt = getFileExtension(sourceFile.getName());
-        if (StringUtils.isEmpty(fileNameExt)) {
-            return FileType.OTHERS;
-        }
-        FileType fileType = FileType.fileTypeMap.get(fileNameExt.toUpperCase());
-        if (fileType == null) {
-            return FileType.OTHERS;
-        }
-        return fileType;
-    }
-
-    public static String getFileExtension(String fileName) {
-        int lastIndexOf = fileName.lastIndexOf(".");
-        if (lastIndexOf == -1) {
-            return "";
-        }
-        return fileName.substring(lastIndexOf + 1);
-    }
-
-    /**
-     * Reference from WorkbookFactory.create(InputStream inp, String password)
-     * @param inputStream
-     * @return
-     */
-    public static FileType getFileTypeByInputStream(InputStream inputStream) {
-        if (inputStream == null) {
-            return FileType.NONE;
-        }
-        InputStream is = FileMagic.prepareToCheckMagic(inputStream);
-        FileMagic fm = null;
-        try {
-            fm = FileMagic.valueOf(is);
-        } catch (IOException e) {
-            throw new EqualException(e);
-        }
-        switch (fm) {
-            case OLE2:
-                return FileType.XLS;
-            case OOXML:
-                return FileType.XLSX;
-            default:
-                return FileType.CSV;
-        }
-    }
-
-    public static Workbook getWorkbook(File file) throws EqualException {
-        try {
-            return WorkbookFactory.create(file);
-        } catch (IOException e) {
-            throw new EqualException(e);
-        }
-    }
-
-    public static Workbook getWorkbook(InputStream inputStream) throws EqualException {
-        try {
-            return WorkbookFactory.create(inputStream);
-        } catch (IOException e) {
-            throw new EqualException(e);
-        }
-    }
-
-    public static byte[] toByteArray(InputStream inputStream) throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024 * 4];
-        int n = 0;
-        while (-1 != (n = inputStream.read(buffer))) {
-            outputStream.write(buffer, 0, n);
-        }
-        outputStream.flush();
-        outputStream.close();
-        return outputStream.toByteArray();
     }
 }
